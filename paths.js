c8f5f7321d5b1acfc625099bda6d1d14f265c333
Brandon Bussjaeger <oppiheht@gmail.com>
2017-01-07 15:51:03 -0800

    Adding red and blue starting positions, and default red and blue paths

---
+++ paths.js
@@ -0,0 +1,14 @@
+var paths = {
+    blue: "drive 16\nface 45\ndrive 48\nface 90\ndrive 7",
+    red: "drive 4\nface 315\ndrive 62\nface 270\ndrive 7"
+}
+var startPos = {
+    blue: {
+        x: 56,
+        y: 9
+    },
+    red: {
+        x: 79,
+        y: 9
+    }
+}
--- sim.js
+++ sim.js
@@ -1,19 +1,25 @@
 var FIELD_SIZE = 144;
 var ROBOT_SIZE = 18;
 var ctx, canvas;
-var pos = {x: 63, y: 9};
-var angle = 0;
+var robot = {
+    pos: {
+        x: 0, 
+        y: 0
+    },
+    angle: 0
+}
 
 window.onload = function() {
-    document.getElementById('commands').value = 'drive 5\nface 315\ndrive 20'
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     ctx.scale(2, 2);
-    reset();
+    drawField();
 }
 
 var run = function() {
-    reset();
+    drawField();
+    setStartPosition();
+    drawRobot();
     var commands = document.getElementById('commands').value;
     commands = commands.split(/\r?\n/);
 
@@ -21,43 +27,47 @@ var run = function() {
         if (commands[command].includes('face')) {
             face(commands[command].match(/\d+/g));
             drawRobot();
-
         }
         if (commands[command].includes('drive')) {
             drive(commands[command].match(/\d+/g));
             drawRobot();
         }
     }
-    document.getElementById('result').innerHTML = "{"+pos.x.toFixed(2) + ", " + pos.y.toFixed(2) + "}";
+    document.getElementById('result').innerHTML = "{"+robot.pos.x.toFixed(2) + ", " + robot.pos.y.toFixed(2) + "}";
 }
 
-function reset() {
-    pos = {x: 63, y: 9};
-    angle = 0;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawField();
-    drawRobot();
+function setStartPosition() {
+    if (document.getElementById('blueRadio').checked) {
+        robot.pos.x = startPos.blue.x;
+        robot.pos.y = startPos.blue.y;
+    }
+    else {
+        robot.pos.x = startPos.red.x;
+        robot.pos.y = startPos.red.y;
+    }
+    robot.angle = 0;
 }
 
 function face(val) {
-    angle = val;
+    robot.angle = val;
 }
 
 function drive(distance) {
-    pos.x -= distance * Math.sin(angle * Math.PI / 180);
-    pos.y += distance * Math.cos(angle * Math.PI / 180);
+    robot.pos.x -= distance * Math.sin(robot.angle * Math.PI / 180);
+    robot.pos.y += distance * Math.cos(robot.angle * Math.PI / 180);
 }
 
 function drawRobot() {
     ctx.save();
     ctx.strokeStyle = "rgb(0, 0, 255)";
-    ctx.translate(pos.x, pos.y);
-    ctx.rotate(angle * Math.PI / 180);
+    ctx.translate(robot.pos.x, robot.pos.y);
+    ctx.rotate(robot.angle * Math.PI / 180);
     ctx.strokeRect(-ROBOT_SIZE/2, -ROBOT_SIZE/2,  ROBOT_SIZE, ROBOT_SIZE);
     ctx.restore();
 }
 
 function drawField() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawGrid();
 }
 
@@ -74,5 +84,12 @@ function drawGrid() {
         ctx.lineTo(FIELD_SIZE, i);
         ctx.closePath();
         ctx.stroke();
+    }
+}
+
+function radioClick(value) {
+    var commands = document.getElementById('commands');
+    if (commands.value.length == 0) {
+        commands.value = paths[value];
     }
 }
